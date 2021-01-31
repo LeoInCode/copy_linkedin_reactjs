60f1dcd4a4513f1d405922012fb99f64d6a65efb
Leonardo Mendes <lsm.ccomp@gmail.com>
2021-01-31 11:48:00 -0300

    Adicionando link de Anúncio

--- src/pages/home.js
+++ src/pages/home.js
@@ -1,5 +1,5 @@
 import React from 'react'
-import { Box, Container, makeStyles, Grid, Hidden } from '@material-ui/core';
+import { Box, Container, makeStyles, Grid, Hidden, Link, Typography } from '@material-ui/core';
 import AppBarComponent from '../component/app-bar'
 import CardLeft from '../component/card-left'
 import CardPublish from '../component/card-publish'
@@ -19,6 +19,22 @@ const useStyles = makeStyles((theme) => ({
     boxShadow: 'none',
     border: '1px solid #0002',
   },
+  link: {
+    color: theme.palette.type === 'dark' ? '#fff' : '#000000e6',
+    textDecoration: 'underline',
+    fontWeight: 'bold',
+    display: 'flex',
+    position: 'absolute',
+    right: '35%',
+    top: '8%'
+  },
+  typography: {
+    color: theme.palette.secondary.main,
+    textDecoration: 'underline',
+    fontWeight: 'bold',
+    display: 'flex',
+    alignItems: 'center'
+  },
   container: {
     margin: theme.spacing(6, 0, 0, 17)
   },
@@ -31,6 +47,12 @@ function Home({ darkMode, setDarkMode }) {
   return (
     <Box className={classes.root}>
       <AppBarComponent darkMode={darkMode} setDarkMode={setDarkMode}/>
+      <Link component="button" variant="body2" className={classes.link}>
+        <Typography variant="body2" className={classes.typography}>
+          Leonardo Mendes -&nbsp;
+        </Typography>
+        Projetando e clonando interfaces do seu gosto
+      </Link>
       <Container className={classes.container}>
         <Grid container spacing={3}>
         <Hidden smDown>
